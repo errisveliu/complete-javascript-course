e55d82e63580f706c0e51f3899ac30e5e65a41b7
Erris Veliu <71988196+errisveliu@users.noreply.github.com>
2022-08-10 14:36:28 +0200

    Update script.js

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -324,7 +324,7 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
   console.log('Someone else should drive..')
 }
 
-*/////////////////////////////////////
+
 
 // const  scoreDolphins = (96 + 108 + 89) / 3;
 // const scoreKoalas = (88 + 91 + 110) / 3;
@@ -353,3 +353,77 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
 } else {
   console.log('No one wins the trophy😔');
 }
+
+
+
+const day = 'wednesday';
+
+switch(day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+    case 'tuesday':
+      console.log('Prepare theory videos');
+      break;
+      case 'wednesday':
+      case 'thursday':
+        console.log('Write code examples');
+        break;
+        case 'friday':
+          console.log('Record videos');
+          break;
+          case 'saturday':
+            case 'sunday':
+              console.log('Enjoy the weekend :D');
+              break;
+              default:
+                console.log('Not a valid day!')
+}
+
+
+if (day === 'monday') {
+  console.log('Plan course structure');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') 
+{
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day!')
+} 
+
+
+3 + 4
+1991
+true && false && !false
+
+if (23>10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old.`)
+*/////////////////////////////////////
+
+const age = 23;
+// age >= 18 ? console.log('I Like to drink Wine🍷') :
+// console.log('I Like to drink Water💧');
+
+const drink = age >= 18 ? 'wine 🍷' : 'water 💧';
+console.log(drink);
+
+let drink2;
+if(age >= 18) {
+  drink2 = 'wine 🍷';
+} else {
+  drink2 = 'water 💧';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine 🍷' : 'water 💧'}`)
